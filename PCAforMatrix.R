05c5cb44b3bafb0557e4185fd79fb5e45dd974bb
albertwcheng <albertwcheng@gmail.com>
2010-11-26 00:05:53 -0500

    added more descriptions for PCAforMatrix.R

--- PCAforMatrix.R
+++ PCAforMatrix.R
@@ -6,7 +6,7 @@ args <- commandArgs(TRUE)
 largs <- length(args)
 
 if(largs<2){
-	cat("Usage: Rscript PCAforMatrix.R matrixFileName outputDir center=yes scale=yes sep=tab showMaxPC=20 drawcomponents=1,2 ...\n")
+	cat("Usage: Rscript PCAforMatrix.R matrixFileName outputDir [center:yes] [scale:yes] [sep:tab] [showMaxPC:20] [ [drawcomponents:1,2] ...]\n")
 	cat("Descriptions:\n")
 	cat("\tPerform PCA analysis on a matrix file where data vectors are in columns. Output to outputDir\n")
 	cat("\nOptions Descriptions:\n")
