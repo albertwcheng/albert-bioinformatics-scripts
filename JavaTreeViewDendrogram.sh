47174e26e2e9a9315e514e8b584f25d451a8f74a
albertwcheng <albertwcheng@gmail.com>
2011-02-12 16:56:45 -0500

    commit

--- JavaTreeViewDendrogram.sh
+++ JavaTreeViewDendrogram.sh
@@ -2,6 +2,7 @@
 
 if [[ $# != 4 ]]; then
 	echo $0 "cdtFile outFile scale[e.g., 10x10] contrast[e.g., 1.0]"
+	exit
 fi
 
 cdtFile=$1
@@ -9,4 +10,6 @@ outFile=$2
 scale=$3
 contrast=$4
 
+
+echo JAVATREEVIEW at  $JAVATREEVIEWPATH
 java -jar $JAVATREEVIEWPATH -r $cdtFile -- -o $outFile -s $scale -a 0 -c $contrast
