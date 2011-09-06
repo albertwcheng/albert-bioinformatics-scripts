6430b5d5a70692b9b0ebf28fe4b5ae1275eb5996
albertwcheng <albertwcheng@gmail.com>
2011-09-06 00:43:56 -0400

    commit

--- CDTToMatrix.sh
+++ CDTToMatrix.sh
@@ -14,5 +14,5 @@ OutMatrix=${CDTFile/.cdt/}
 OutMatrix=${OutMatrix/.CDT/}.mat
 fi
 
-cuta.py -f1,4-_1 $CDTFile | awk 'FNR!=2 && FNR!=3' > $OutMatrix
+cuta.py -f1,4-_1 $CDTFile | awk '$1!="AID" && $1!="EWEIGHT"' > $OutMatrix #'FNR!=2 && FNR!=3'
 
