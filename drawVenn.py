d51263698cec820f717b8f0bc865ae19a204c7fe
albertwcheng <albertwcheng@gmail.com>
2012-02-23 02:53:11 -0500

    commit

--- drawVenn.py
+++ drawVenn.py
@@ -20,16 +20,21 @@ def twodigithex(I):
 	return hexString.upper()
 	
 
-def drawTwoVenn(set1,set2,intersect,width,height,outfile,title=None,legend=None,indicateNumberInLegend=False,colors=None):
+def drawTwoVenn(set1,set2,intersect,width,height,outfile,title=None,legend=None,indicateNumberInLegend=False,colors=None,htmlReport=None):
 	chart=VennChart(width,height)
 	chart.add_data([set1,set2,0,intersect])
 	if title:
 		chart.set_title(title)
 	if legend:
+	
+	
+		legendOut=legend[:]
+			
 		if indicateNumberInLegend:
-			legend[0]=legend[0]+" ("+str(set1)+")"
-			legend[1]=legend[1]+" ("+str(set2)+")"
-		chart.set_legend(legend)
+			
+			legendOut[0]=legend[0]+" ("+str(set1)+")"
+			legendOut[1]=legend[1]+" ("+str(set2)+")"
+		chart.set_legend(legendOut)
 	if colors:
 		for i in range(0,len(colors)):
 			if type(colors[i]) is not StringType:
@@ -45,20 +50,33 @@ def drawTwoVenn(set1,set2,intersect,width,height,outfile,title=None,legend=None,
 		#print >> stderr,colors
 		chart.set_colours(colors)
 	
+	
 	chart.download(outfile)
+	
+	if htmlReport:
+		if not title:
+			title=""
+		if not legend:
+			legend=["set1","set2"]
+			
+		fout = open(htmlReport,"w")
+		print >> fout, '<html><head><title>%s</title></head><body><h1>%s</h1><table width=%d><tr><td colspan=2><img src="%s"></td></tr><tr bgcolor="#AAAAAA"><td>%s</td><td>%d</td></tr><tr><td>%s</td><td>%d</td></tr> <tr bgcolor="#AAAAAA">  <td>%s only</td><td>%d</td></tr> <tr><td>%s only</td><td>%d</td></tr> <tr bgcolor="#AAAAAA"> <td>%s &#x2229; %s</td><td>%d</td></tr> </body></html>' %(title,title,width,outfile,legend[0],set1,legend[1],set2,legend[0],set1-intersect,legend[1],set2-intersect,legend[0],legend[1],intersect)
+		fout.close()
 
-def drawThreeVenn(set1,set2,set3,set1set2Intersect,set1set3Intersect,set2set3Intersect,allIntersect,width,height,outfile,title=None,legend=None,indicateNumberInLegend=False,colors=None):
+def drawThreeVenn(set1,set2,set3,set1set2Intersect,set1set3Intersect,set2set3Intersect,allIntersect,width,height,outfile,title=None,legend=None,indicateNumberInLegend=False,colors=None,htmlReport=None):
 	chart=VennChart(width,height)
 	chart.add_data([set1,set2,set3,set1set2Intersect,set1set3Intersect,set2set3Intersect,allIntersect])
 	if title:
 		chart.set_title(title)
 	if legend:
+		legendOut=legend[:]
+		
 		if indicateNumberInLegend:
-			legend[0]=legend[0]+" ("+str(set1)+")"
-			legend[1]=legend[1]+" ("+str(set2)+")"
-			legend[2]=legend[2]+" ("+str(set3)+")"
+			legendOut[0]=legend[0]+" ("+str(set1)+")"
+			legendOut[1]=legend[1]+" ("+str(set2)+")"
+			legendOut[2]=legend[2]+" ("+str(set3)+")"
 			
-		chart.set_legend(legend)
+		chart.set_legend(legendOut)
 		
 	if colors:
 		for i in range(0,len(colors)):
@@ -77,16 +95,30 @@ def drawThreeVenn(set1,set2,set3,set1set2Intersect,set1set3Intersect,set2set3Int
 	
 	chart.download(outfile)
 
+	if htmlReport:
+		fout = open(htmlReport,"w")
+		set1only=set1-set1set2Intersect-set1set3Intersect+allIntersect
+		set2only=set2-set1set2Intersect-set2set3Intersect+allIntersect
+		set3only=set3-set1set3Intersect-set2set3Intersect+allIntersect
+		
+		if not title:
+			title=""
+		if not legend:
+			legend=["set1","set2","set3"]
+		
+		print >> fout, '<html><head><title>%s</title></head><body><h1>%s</h1><table width=%d><tr><td colspan=2><img src="%s"></td></tr><tr bgcolor="#AAAAAA"><td>%s</td><td>%d</td></tr><tr><td>%s</td><td>%d</td></tr> <tr bgcolor="#AAAAAA">  <td>%s</td><td>%d</td></tr> <tr>  <td>%s only</td><td>%d</td></tr>  <tr bgcolor="#AAAAAA"> <td>%s only</td><td>%d</td> </tr>  <tr><td>%s only</td><td>%d</td> </tr> <tr bgcolor="#AAAAAA"> <td>%s &#x2229; %s</td><td>%d</td></tr> <tr> <td>%s &#x2229; %s</td><td>%d</td></tr> <tr bgcolor="#AAAAAA"> <td>%s &#x2229; %s</td><td>%d</td></tr> <tr> <td>%s &#x2229; %s &#x2229; %s </td><td>%d</td></tr> </body></html>' %(title,title,width,outfile,legend[0],set1,legend[1],set2,legend[2],set3,legend[0],set1only,legend[1],set2only,legend[2],set3only,legend[0],legend[1],set1set2Intersect,legend[0],legend[2],set1set3Intersect,legend[1],legend[2],set2set3Intersect,legend[0],legend[1],legend[2],allIntersect)
+		fout.close()
+
 
 def testTwoVenn():
-	drawTwoVenn(10,10,5,500,300,"testTwoVenn.png","two venn testing",["A","B"],True,[[255,255,0],[0,255,255]])
+	drawTwoVenn(10,20,3,500,300,"testTwoVenn.png","Two venn testing",["A","B"],True,[[255,255,0],[0,255,255]],"testTwoVenn.htm")
 	
 def testThreeVenn():
-	drawThreeVenn(100,80,60,30,30,30,10,500,300,"testThreeVenn.png","three venn testing",["A","B","C"],True,[[255,255,0],[0,255,255],[255,0,0]])
+	drawThreeVenn(100,80,60,30,30,30,10,500,300,"testThreeVenn.png","Three venn testing",["A","B","C"],True,[[255,255,0],[0,255,255],[255,0,0]],"testThreeVenn.htm")
 	
 
 if __name__=='__main__':
 	#testTwoVenn()
-	#testThreeVenn()
+	testThreeVenn()
 	
 	
