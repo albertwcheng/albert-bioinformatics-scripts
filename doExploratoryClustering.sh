f7d2c3eecbdeea8bc8d1246d6a65fbc297dfb80c
albertwcheng <albertwcheng@gmail.com>
2012-10-17 01:31:23 -0400

    commit

---
+++ doExploratoryClustering.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]; then
+	echo $0 infile jobname
+	exit
+fi
+
+infile=$1
+jobname=$2
+
+runCombinations.py pyClusterArray.py "--top-sd 5000|--top-sd 1000|--top-sd 500|" "--center-gene a|--center-gene m|" "--normalize-gene|" -p -j $jobname $infile "c|s" "a|m"
