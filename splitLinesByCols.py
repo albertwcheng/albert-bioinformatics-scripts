3de5424d48aeddef713d64b3d8af96f694462317
albertwcheng <albertwcheng@gmail.com>
2012-11-06 03:33:57 -0500

    commit

---
+++ splitLinesByCols.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+from albertcommon import *
+from sys import *
+from getopt import getopt
+
+def printUsageAndExit(programName):
+	print >> stderr,programName,"filename outprefix outsuffix cols"
+	explainColumns(stderr)
+	exit(1)
+
+def getOutFileName(outprefix,outsuffix,keys,fnjoiner):
+	return outprefix+fnjoiner.join(keys)+outsuffix
+	
+if __name__=='__main__':
+	programName=argv[0]
+	opts,args=getopt(argv[1:],'',['fnjoiner='])
+	fnJoiner="_"
+	
+	startRow=2
+	headerRow=1
+	fs="\t"
+		
+	for o,v in opts:
+		if o=='--fnjoiner':
+			fnJoiner=v
+	
+	try:
+		filename,outprefix,outsuffix,cols=args
+	except:
+		printUsageAndExit(programName)
+		
+
+	header,prestarts=getHeader(filename,headerRow,startRow,fs)
+	cols=getCol0ListFromCol1ListStringAdv(header,cols)
+	
+	seen=set()
+	
+	fil=open(filename)
+	
+	lino=0
+	for lin in fil:
+		lin=lin.rstrip("\r\n")
+		lino+=1
+		if lino<startRow:
+			continue
+		
+		fields=lin.split(fs)
+		keys=tuple(getSubvector(fields,cols))
+		outname=getOutFileName(outprefix,outsuffix,keys,fnJoiner)
+		if keys not in seen:
+			#new file
+			seen.add(keys)
+			ofil=open(outname,"w")
+			for prestart in prestarts:
+				print >> ofil,fs.join(prestart)
+		
+		else:
+			ofil=open(outname,"a")
+		
+		print >> ofil,lin
+			
+		ofil.close()
+		
+			
+		
+	fil.close()
+	
+	
