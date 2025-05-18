8484f24ea75b376d5ba2dd75491e8b36f3ed1777
Albert Cheng <albert@ALBERTASUS>
2025-05-18 21:00:44 +0800

    commit

---
+++ speakSeq.py
@@ -0,0 +1,25 @@
+#!/usr/local/env python3
+
+from sys import *
+from gtts import gTTS
+
+def printUsageAndExit():
+    print(argv[0],"seq out_mp3",file=stderr)
+    exit(1)
+
+if __name__=='__main__':
+    try:
+        seq,out_mp3=arg[1:]
+    except:
+        printUsageAndExit()
+
+    textToSpeak=""
+
+    for s in seq:
+        textToSpeak+=s+". "
+
+    tts=gTTS(textToSpeak,lang='en')
+    tts.save(out_mp3)
+
+
+
